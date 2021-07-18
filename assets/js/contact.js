186611406059c653f12b773481d25f95eecb155d
unknown <darian.sopi@gmail.com>
2021-07-18 17:25:35 +0300

    Website creation

---
+++ assets/js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "Introdu raspunsul corect -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Introduceti Nume Prenume",
+                    minlength: "Nume Prenume trebuie sa contina minim 2 caractere"
+                },
+                subject: {
+                    required: "Introduceti Subiectul",
+                    minlength: "Subiectul trebuie sa contina minim 4 caractere"
+                },
+                number: {
+                    required: "Introduceti Numarul",
+                    minlength: "Numarul trebuie sa contina minim 5 caractere"
+                },
+                email: {
+                    required: "Introduceti Emailul"
+                },
+                message: {
+                    required: "Introduceti Mesajul",
+                    minlength: "Mesajul trebuie sa contina minim 20 caractere"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
